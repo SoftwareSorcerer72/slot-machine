75f4e56df75ac482a9871a7c88869bb0756c5c44
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-29 06:16:51 -0400

    Set up deposit function, get_number_of_lines function, and defined global variables of MAX_BETS, MIN_BETS, and MAX_LINES

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+MAX_LINES = 3
+MAX_BETS = 100
+MIN_BET = 1
+
 def deposit():
     while True:
-        amount = input("what would you like to deposit? $")
+        amount = input("What would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -9,7 +13,30 @@ def deposit():
                 print("Amount must be greater than 0.")
         else:
             print("Please enter a number.")
-            
+
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return lines    
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+
+
+main()
